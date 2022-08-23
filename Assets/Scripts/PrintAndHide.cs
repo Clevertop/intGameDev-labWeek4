f103f88eec59c28b6ef70d3357d5307dc445bf58
Clevertop <41929769+Clevertop@users.noreply.github.com>
2022-08-23 12:31:13 +1000

    100 percent

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     int i = 3;
+    int rand;
     public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rand = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -17,5 +18,13 @@ public class PrintAndHide : MonoBehaviour
     {
         i += 1;
         print(gameObject.name + ":" + i);
+        if (gameObject.tag == "Red" && i >= 100)
+        {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag == "Blue" && i >= rand)
+        {
+            rend.enabled = false;
+        }
     }
 }
